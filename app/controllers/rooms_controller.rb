d7a022e4f3a981056e9b77e90b540f0f5371cba4
OKI-2024 <tetsuokinokun@gmail.com>
2024-03-18 14:52:35 +0900

    チャットルーム作成機能の実装

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -6,6 +6,21 @@ def new
 
 end
 
+def create
+
+  @room = Room.new(room_params)
+    if @room.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entitiy
+    end
+end
+
+private
+
+def room_params
+  params.require(:room).permit(:name, user_ids: [])
+end
 
 
 end
